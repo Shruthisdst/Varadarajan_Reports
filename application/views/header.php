b107ffb16aaa02789246d9c7bbdf8d9c503761c8
Shruthisdst <shruthitr.nayak@gmail.com>
2016-06-06 11:06:00 +0530

    No. 3 - updated search view

--- application/views/header.php
+++ application/views/header.php
@@ -4,7 +4,7 @@
     <!-- Basic Page Needs
     –––––––––––––––––––––––––––––––––––––––––––––––––– -->
     <meta charset="utf-8">
-    <title><?php if($pageTitle) echo $pageTitle . ' | '; ?>Indian Academy of Sciences</title>
+    <title><?php if($pageTitle) echo $pageTitle . ' | '; ?>Varadarajan Reports</title>
     <meta name="description" content="">
     <meta name="author" content="">
 
